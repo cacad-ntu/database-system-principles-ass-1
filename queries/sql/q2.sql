03f7608fe779be8e00a8ca0636940a592de60bc2
cacad101 <gbbpentium@gmail.com>
2017-10-16 17:55:11 +0800

    update queries

--- queries/sql/q2.sql
+++ queries/sql/q2.sql
@@ -1,10 +1,6 @@
-    
-    SELECT      DISTINCT upper(t.title_abbrev)
-    FROM        (
-                SELECT      p.title_abbrev, p.year, count(p.title_abbrev) as cnt
-                FROM        publication as p
-                WHERE       p.pub_class = 'inproceedings' AND 
-                            p.month = 7
-                GROUP BY    p.title_abbrev, p.year
-                ) as t
-    WHERE t.cnt > 200
+SELECT      DISTINCT upper(p.title_abbrev)
+FROM        publication as p
+WHERE       p.pub_class = 'inproceedings' AND 
+            p.month = 7
+GROUP BY    p.title_abbrev, p.year
+HAVING count(p.title_abbrev) > 200
